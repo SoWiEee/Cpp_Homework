d824b8f8ae91c6cc594519ecf89539e72cae22fe
SoWiEee <kawiisowieee@gmail.com>
2024-05-19 15:17:18 +0800

    homework upload

--- Q8_7.cpp
+++ Q8_7.cpp
@@ -1,137 +1,69 @@
 #include <iostream>
-#include<cstdlib>
-#include<cmath>
 
 using namespace std;
 
-class PrimeNumber{
+class PrimeNumber {
 private:
     int prime;
 
 public:
     // constructor
-    PrimeNumber(): prime(1){}
-    PrimeNumber(int p): prime(p){}
+    PrimeNumber() : prime(1) {}
+    PrimeNumber(int p) : prime(p) {}
 
-    bool isPrime(int number) const {
-        if(number == 2 || number == 3)
-            return true;
-        else if(number > 4){
-
-            int remainder = number % 6;
-
-            if(remainder != 1 && remainder != 5)
-                return false;
-
-            int nSqrt = (int)floor(sqrt(number));
-
-            for(int i = 5; i <= nSqrt; i++) {
-                if(number % i == 0 || number % (i + 2) == 0)
-                    return false;
-            }
-            return true;
-        }
-        else{
-            return false;
-        }
+    int getPrime() const {
+        return prime;
     }
 
-    void setPrime(int p){
-        while(!isPrime(p)){
-            p--;
-        }
-        prime = p;
+    bool isPrime(int num) {
+        if (num <= 1) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i * i <= num; i += 2)
+            if (num % i == 0) return false;
+        return true;
     }
 
-    PrimeNumber operator++() {
-
-        prime++;
-        while(!isPrime(prime))
-            prime++;
-
-        return PrimeNumber(prime);
-
+    // overloading function
+    PrimeNumber& operator++() {
+        int num = prime + 1;
+        while (!isPrime(num)) num++;
+        prime = num;
+        return *this;
     }
 
     PrimeNumber operator++(int) {
-
-        int temp = prime;
-
-        prime++;
-        while(!isPrime(prime))
-            prime++;
-
-        if(temp == 1)
-            return PrimeNumber();
-        else
-            return PrimeNumber(temp);
-
+        PrimeNumber temp = *this;
+        ++*this;
+        return temp;
     }
 
-    PrimeNumber operator--() {
-
-        if(prime <= 2) {
+    PrimeNumber& operator--() {
+        if (prime <= 2) {
             prime = 1;
-            return PrimeNumber();
+            return *this;
         }
-        else{
-            prime--;
-            while(!isPrime(prime))
-                prime--;
-        }
-
-        return PrimeNumber(prime);
-
+        int num = prime - 1;
+        while (!isPrime(num)) num--;
+        prime = num;
+        return *this;
     }
 
     PrimeNumber operator--(int) {
-
-        int temp = prime;
-
-        if(temp < 2)
-            return PrimeNumber();
-        else if(temp == 2) {
-            prime = 1;
-            return PrimeNumber(temp);
-        }
-        else{
-            prime--;
-            while(!isPrime(prime))
-                prime--; 
-        }
-
-        return PrimeNumber(temp);
-
-    }
-
-    friend istream& operator >>(istream& is, PrimeNumber& primeNumber) {
-
-        int num;
-
-        do{
-            is >> num;
-        }while(num < 2);
-
-        primeNumber.setPrime(num);
-
-        return is;
-
-    }
-
-    friend ostream& operator <<(ostream& outputStream, const PrimeNumber& primeNumber) {
-
-        outputStream << primeNumber.prime;
-        
-        return outputStream;
-
+        PrimeNumber temp = *this;
+        --*this;
+        return temp;
     }
 };
 
-
-int main(){
-
+int main(void) {
+    PrimeNumber p(13);
+    cout << "Prime: " << p.getPrime() << endl;
+    ++p;
+    cout << "Next Prime: " << p.getPrime() << endl;
+    p--;
+    cout << "Previous Prime: " << p.getPrime() << endl;
 
     system("pause");
     return 0;
 }
-
